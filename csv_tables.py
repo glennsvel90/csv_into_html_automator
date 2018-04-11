15801faacfb76b8159597c9e56327979939259f4
glennsvel90 <glennselwynvel@gmail.com>
2018-04-11 16:11:06 -0400

    insert csv function

--- csv_tables.py
+++ csv_tables.py
@@ -12,32 +12,15 @@ def insert_csv(html_path, csv_paths):
     new_table = ''
     for path in csv_paths:
         print('Building table from' + path)
+        file = open(path, 'r')
+        csv_data = list(csv.reader(file, delimiter=','))
+        new_table += create_table(csv_data, line_space = sp + 2)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    print('Inserting tables into copy of ' + html_path)
+    output_html = html_halves[0].rstrip(' ') + new_table + (sp * ' ') + '</article>' + html_halves[1]
+    output_path = os.path.dirname(html_path) + '/output.htm'
+    print('Saving result to ' + output_path)
+    output_file = open(output_path, 'w')
+    output_file.write(output_html)
 
 if __main__=="__main__": insert_csv(HTML_PATH, CSV_FILES)
